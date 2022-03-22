eb8a0a8d174d07de6c0f13de5b4e56c2a23a1021
angierbls <amorobles99@gmail.com>
2022-03-22 20:41:19 +0800

    testing sa logo123

---
+++ app/src/main/java/com/example/emongani/logo.java
@@ -0,0 +1,16 @@
+package com.example.emongani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
+
+//testing sa logo activity
