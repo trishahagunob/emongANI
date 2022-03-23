b914bbb9225748811d42b0c24250b84f939d86f1
Ina Alegrado <ipalegrado@usep.edu.ph>
2022-03-23 20:36:29 +0800

    added my profile_seller activity

---
+++ app/src/main/java/com/example/emongani/MyProfile_Seller.java
@@ -0,0 +1,14 @@
+package com.example.emongani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyProfile_Seller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile_seller);
+    }
+}
